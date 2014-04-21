1c577847e145b4b1082d299f822e91e8f20a2b0d
Chao-Ping Lin <chaopinglin@users.noreply.github.com>
2014-04-21 00:18:03 -0700

    Create a new RoR project
    $ rails new Ruby_on_Rails_Tutorial --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyOnRailsTutorial::Application.config.secret_key_base = '1ae73c471dd84fc550a81fe02863228a9ff88878d0593c8243ca5ae9b4f96e82f3821874c9838754c8b62b8137018db00738bb8a77ba7d6a7cf10dde6caaae01'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyOnRailsTutorial::Application.config.session_store :cookie_store, key: '_Ruby_on_Rails_Tutorial_session'
